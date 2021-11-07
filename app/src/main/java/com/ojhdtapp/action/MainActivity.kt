6389c7cb287fa7c3ac60469af5e53da74ae96c45
ojhdt <ojhdtmail@gmail.com>
2021-11-07 14:40:03 +0800

    update

--- app/src/main/java/com/ojhdtapp/action/MainActivity.kt
+++ app/src/main/java/com/ojhdtapp/action/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.ojhdtapp.action
 
+import android.app.ActivityOptions
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ojhdtapp.action.databinding.ActivityMainBinding
@@ -8,7 +10,5 @@ class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
     }
 }
