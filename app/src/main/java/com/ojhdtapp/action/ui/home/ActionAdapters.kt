9cb1df2ca5e362c37ee9b9bbe2df08eefdc665c2
ojhdt <ojhdtmail@gmail.com>
2021-11-09 15:27:29 +0800

    update

---
+++ app/src/main/java/com/ojhdtapp/action/ui/home/ActionAdapters.kt
@@ -0,0 +1,11 @@
+package com.ojhdtapp.action.ui.home
+
+import android.view.View
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+class HeadlineAdapter : ListAdapter<_,HeadlineViewHolder>(){}
+
+class HeadlineViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+    val
+}
