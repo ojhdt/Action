fee6bc738a5ce495d40debf0a4662301531b022c
ojhdt <ojhdtmail@gmail.com>
2021-11-15 15:09:58 +0800

    update

--- app/src/main/java/com/ojhdtapp/action/ui/ProgressView.kt
+++ app/src/main/java/com/ojhdtapp/action/ui/ProgressView.kt
@@ -4,25 +4,51 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.util.AttributeSet
+import android.util.Log
 import android.util.TypedValue
 import android.view.View
+import androidx.core.graphics.withTranslation
 import com.ojhdtapp.action.R
 
 class ProgressView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
+    init {
+        context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.ProgressView,
+            0, 0
+        ).apply {
+            try {
+                progress = getInteger(R.styleable.ProgressView_progress, 0)
+                paintColor = getColor(R.styleable.ProgressView_color, TypedValue().apply {
+                    context.theme.resolveAttribute(
+                        android.R.attr.colorPrimary,
+                        this,
+                        true
+                    )
+                }.data)
+            } finally {
+                recycle()
+            }
+        }
+    }
+
     private var mwidth = 0f
     private var mheight = 0f
-    private var progress = 0
+    private var progress: Int? = null
+        get() = field
+        set(value) {
+            field = value
+        }
+    private var paintColor: Int? = null
+        get() = field
+        set(value) {
+            field = value
+        }
     private val fillcolorPrimaryPaint = Paint().apply {
         style = Paint.Style.FILL
-        color = TypedValue().apply {
-            context.theme.resolveAttribute(
-                android.R.attr.colorPrimary,
-                this,
-                true
-            )
-        }.data
+        color = paintColor!!
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
@@ -33,5 +59,9 @@ class ProgressView @JvmOverloads constructor(
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
+        canvas?.withTranslation(0f, mheight) {
+            val currentHeight = mheight * ((progress!!.toFloat()).div(100f))
+            drawRect(0f, -currentHeight, mwidth, 0f, fillcolorPrimaryPaint)
+        }
     }
 }
