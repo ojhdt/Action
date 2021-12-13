42caba4c869c91cdc9e693df9c977b8e8ee24cae
ojhdt <ojhdtmail@gmail.com>
2021-12-13 16:29:16 +0800

    add transition

--- app/src/main/java/com/ojhdtapp/action/ui/archive/ActionArchiveFragment.kt
+++ app/src/main/java/com/ojhdtapp/action/ui/archive/ActionArchiveFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.doOnPreDraw
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
@@ -13,6 +14,7 @@ import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.transition.Hold
 import com.google.android.material.transition.MaterialSharedAxis
 import com.ojhdtapp.action.R
 import com.ojhdtapp.action.databinding.FragmentActionArchiveBinding
@@ -50,6 +52,9 @@ class ActionArchiveFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        // Postpone Transition
+        postponeEnterTransition()
+        view.doOnPreDraw { startPostponedEnterTransition() }
         // Setup Appbar
         val navController = findNavController()
         val appBarConfiguration = AppBarConfiguration(navController.graph)
@@ -81,7 +86,12 @@ class ActionArchiveFragment : Fragment() {
             false
         }
         // Adapter for rv
-        val myAdapter = ActionArchiveAdapter()
+        val myAdapter = ActionArchiveAdapter(object: ActionArchiveAdapter.ActionArchiveListener{
+            override fun onActionClick() {
+                exitTransition = Hold()
+                reenterTransition = Hold()
+            }
+        })
         binding.recyclerView.run {
             adapter = myAdapter
             layoutManager = GridLayoutManager(context,2)
--- app/src/main/java/com/ojhdtapp/action/ui/archive/ActionArchiveHistoryFragment.kt
+++ app/src/main/java/com/ojhdtapp/action/ui/archive/ActionArchiveHistoryFragment.kt
@@ -1,32 +1,43 @@
 package com.ojhdtapp.action.ui.archive
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.TypedValue
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.ViewCompat
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.transition.MaterialContainerTransform
 import com.ojhdtapp.action.R
+import com.ojhdtapp.action.databinding.FragmentActionArchiveHistoryBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ActionArchiveHistoryFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ActionArchiveHistoryFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private var _binding: FragmentActionArchiveHistoryBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
+        }
+        // Add Transition
+        sharedElementEnterTransition = MaterialContainerTransform().apply {
+            drawingViewId = R.id.nav_host
+            duration = resources.getInteger(R.integer.material_motion_duration_long_1).toLong()
+            scrimColor = Color.TRANSPARENT
+            setAllContainerColors(TypedValue().apply {
+                context?.theme?.resolveAttribute(
+                    android.R.attr.colorBackground,
+                    this,
+                    true
+                )
+            }.data)
         }
     }
 
@@ -35,26 +46,38 @@ class ActionArchiveHistoryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_action_archive_history, container, false)
+        _binding = FragmentActionArchiveHistoryBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ActionArchiveHistoryFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ActionArchiveHistoryFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Share Element Transition
+        ViewCompat.setTransitionName(
+            binding.actionHistoryContentContainer, getString(
+                R.string.action_history_transition_name
+            )
+        )
+        // Setup Appbar
+        val navController = findNavController()
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        NavigationUI.setupWithNavController(
+            binding.toolbar,
+            navController,
+            appBarConfiguration
+        )
+        binding.toolbar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.refresh -> {}
+                R.id.toContent -> {}
+                else -> {}
             }
+            false
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
