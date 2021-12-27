63858ffc898947e221d033a2163abea205524100
ojhdt <ojhdtmail@gmail.com>
2021-12-27 22:43:22 +0800

    add toolbar to settingfrag

--- app/src/main/java/com/ojhdtapp/action/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/ojhdtapp/action/ui/settings/SettingsFragment.kt
@@ -1,12 +1,51 @@
 package com.ojhdtapp.action.ui.settings
 
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.preference.PreferenceFragmentCompat
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.transition.MaterialSharedAxis
 import com.ojhdtapp.action.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Setup Transition
+        enterTransition = MaterialSharedAxis(MaterialSharedAxis.Z, true).apply {
+            duration = resources.getInteger(R.integer.material_motion_duration_long_1).toLong()
+        }
+        returnTransition = MaterialSharedAxis(MaterialSharedAxis.Z, false).apply {
+            duration = resources.getInteger(R.integer.material_motion_duration_long_1).toLong()
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Setup Appbar
+        val toolbar = view.findViewById<MaterialToolbar>(R.id.toolbar)
+        val navController = findNavController()
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        toolbar?.let{
+            NavigationUI.setupWithNavController(
+                it,
+                navController,
+                appBarConfiguration
+            )
+//            it.setOnMenuItemClickListener {
+//                when (it.itemId) {
+//                    else -> {}
+//                }
+//                false
+//            }
+        }
     }
 }
