2d7991cd9410dd67953067e0aecaf6c50fdf099c
ojhdt <ojhdtmail@gmail.com>
2022-01-06 18:32:59 +0800

    Update PermissionsFragment.kt

--- app/src/main/java/com/ojhdtapp/action/ui/settings/PermissionsFragment.kt
+++ app/src/main/java/com/ojhdtapp/action/ui/settings/PermissionsFragment.kt
@@ -1,24 +1,48 @@
 package com.ojhdtapp.action.ui.settings
 
+import android.Manifest
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.transition.MaterialSharedAxis
 import com.ojhdtapp.action.R
 
 class PermissionsFragment : PreferenceFragmentCompat() {
+    private lateinit var requestPermissionLauncher: ActivityResultLauncher<Array<String>>
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.permissions_preferences, rootKey)
-
+        findPreference<Preference>("all_granted")?.apply {
+            setOnPreferenceClickListener {
+                val permissions = arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.BODY_SENSORS,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+                requestPermissionLauncher.launch(permissions)
+                true
+            }
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Permission
+        requestPermissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+                findPreference<PermissionPreference>("permission_location")?.refreshState()
+                findPreference<PermissionPreference>("permission_sensor")?.refreshState()
+                findPreference<PermissionPreference>("permission_storage")?.refreshState()
+            }
         // Setup Transition
         exitTransition = MaterialSharedAxis(MaterialSharedAxis.Z, true).apply {
             duration = resources.getInteger(R.integer.material_motion_duration_long_1).toLong()
