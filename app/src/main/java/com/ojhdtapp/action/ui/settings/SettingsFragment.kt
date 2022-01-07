2b3074ba488c1337c80c3c33cdcf8fc3e11b962b
ojhdt <ojhdtmail@gmail.com>
2022-01-07 19:29:38 +0800

    notice of dynamicColor switch

--- app/src/main/java/com/ojhdtapp/action/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/ojhdtapp/action/ui/settings/SettingsFragment.kt
@@ -85,7 +85,7 @@ class SettingsFragment : PreferenceFragmentCompat() {
                 isEnabled = false
             } else {
                 setOnPreferenceChangeListener { preference, newValue ->
-                    activity?.recreate()
+                    summary = getString(R.string.dynamic_color_summary_edited)
                     true
                 }
             }
